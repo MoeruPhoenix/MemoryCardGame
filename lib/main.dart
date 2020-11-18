0320948bee222d53a3977510b224a3cd61401ec5
Orrin Reid <orrin.reid@gmail.com>
2020-11-18 09:55:06 -0500

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -18,12 +18,10 @@ void main() {
   ));
 }
 
-int level =  8;
-
 class CardGame extends StatefulWidget {
   final int size;
 
-  const  CardGame({Key key, this.size = 8}) : super(key: key);
+  const CardGame({Key key, this.size = 12}) : super(key: key);
   @override
   _CardGameState createState() => _CardGameState();
 }
@@ -34,114 +32,108 @@ class _CardGameState extends State<CardGame> {
   List<String> data = [];
   int previousIndex = -1;
   bool flip = false;
+
   int time = 0;
   Timer timer;
 
   @override
   void initState() {
     super.initState();
-    for (var i = 0; i <widget.size; i++) {
+    for (var i = 0; i < widget.size; i++) {
       cardStateKeys.add(GlobalKey<FlipCardState>());
       cardFlips.add(true);
     }
-    for (var i = 0; i <widget.size ~/ 2; i++) {
+    for (var i = 0; i < widget.size ~/ 2; i++) {
       data.add(i.toString());
-      cardFlips.add(true);
     }
-    for (var i = 0; i <widget.size ~/ 2; i++) {
+    for (var i = 0; i < widget.size ~/ 2; i++) {
       data.add(i.toString());
-      cardFlips.add(true);
     }
     startTimer();
     data.shuffle();
   }
 
-  startTimer(){
+  startTimer() {
     timer = Timer.periodic(Duration(seconds: 1), (t) {
       setState(() {
         time = time + 1;
       });
     });
-
   }
-
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
-       title: Center(
-         child: Text('Memory Card Game'),
-       ),
-     ),
+      appBar: AppBar(
+        title: Center(
+          child: Text('Memory Card Game'),
+        ),
+      ),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
             children: <Widget>[
               Padding(
-                padding: const EdgeInsets.all(16.0),
+                padding: EdgeInsets.all(16.0),
                 child: Text(
                   "$time",
-                  //ignore: deprecated_member_use
-                  style: Theme.of(context).textTheme.display2,
+                  style: Theme.of(context).textTheme.headline2,
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.all(16.0),
-                child: GridView.builder(
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                  ),
-                  itemBuilder: (context,index)=>FlipCard(
-                    key: cardStateKeys[index],
-                    onFlip: () {
-                      if(!flip) {
-                        flip = true;
-                      }else {
-                        flip = false;
-                        if(previousIndex != index) {
-                          if (data[previousIndex] != data[index]) {
-                            cardStateKeys[previousIndex]
-                                .currentState
-                                .toggleCard();
-                            previousIndex = index;
-                          }else{
-                            cardFlips[previousIndex] = false;
-                            cardFlips[index] = false;
-                            print(cardFlips);
+              Theme(
+                data: ThemeData.dark(),
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: GridView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                    ),
+                    itemBuilder: (context, index) => FlipCard(
+                      key: cardStateKeys[index],
+                      onFlip: () {
+                        if (!flip) {
+                          flip = true;
+                          previousIndex = index;
+                        } else {
+                          flip = false;
+                          if (previousIndex != index) {
+                            if (data[previousIndex] != data[index]) {
+                              cardStateKeys[previousIndex]
+                                  .currentState
+                                  .toggleCard();
+                              previousIndex = index;
+                            } else {
+                              cardFlips[previousIndex] = false;
+                              cardFlips[index] = false;
 
-                            if (cardFlips.every((t) => t == false)) {
-                              print("Successful Match");
-                              showResult();
+                              if (cardFlips.every((t) => t == false)) {
+                                showResult();
+                              }
                             }
                           }
                         }
-                      }
-                    },
-                    direction: FlipDirection.HORIZONTAL,
-                    flipOnTouch: cardFlips[index],
-                    front: Container(
-                      margin: EdgeInsets.all(4.0),
-                      //backgroundImage: AssetImage('images/hiddenbackgorund.jpg'),
-                      color: Color.fromRGBO(63, 81, 181, 0.3),
-                    ),
-                    back: Container(
-                      margin: EdgeInsets.all(4.0),
-                      //backgorundImage: AssetImage('images/hiddenbackgorund.jpg'),
-                      color: Color.fromRGBO(63, 81, 181, 1),
-                      child: Center(
-                        // ignore: deprecated_member_use
-                        child: Text(
-                          // ignore: deprecated_member_use
-                            "$data[index]", style: Theme.of(context).textTheme.display2
+                      },
+                      direction: FlipDirection.HORIZONTAL,
+                      flipOnTouch: cardFlips[index],
+                      front: Container(
+                        margin: EdgeInsets.all(4.0),
+                        color: Color.fromRGBO(63, 81, 181, 1),
+                      ),
+                      back: Container(
+                        margin: EdgeInsets.all(4.0),
+                        color: Color.fromRGBO(48, 63, 159, 1),
+                        child: Center(
+                          child: Text(
+                            "${data[index]}",
+                            style: Theme.of(context).textTheme.headline2,
+                          ),
                         ),
                       ),
                     ),
+                    itemCount: 12,
                   ),
-                  itemCount: data.length,
                 ),
               ),
             ],
@@ -150,35 +142,33 @@ class _CardGameState extends State<CardGame> {
       ),
     );
   }
+
+  //Display Results
   showResult() {
-    showDialog(context: context, barrierDismissible: false, builder: (context) => AlertDialog(
-      title: Text(""
-          "Successful Matching"
-      ),
-      // ignore: deprecated_member_use
-      content: Text(
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: Text("Congrats"),
+        content: Text(
           "Time $time",
-          // ignore: deprecated_member_use
-          style: Theme.of(context).textTheme.display2
-      ),
-      actions: <Widget> [
-        FlatButton(
-          onPressed: () {
-            Navigator.of(context).pushReplacement(
-              MaterialPageRoute(
-                builder: (context) => CardGame(
-                  size: level,
-                ),
-            ),
-            );
-            level *= 2;
-          },
-          child: Text (
-              "NEXT"
-          ),
+          style: Theme.of(context).textTheme.headline2,
         ),
-      ],
-    ),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () {
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                  builder: (context) => CardGame(
+                    size: 12,
+                  ),
+                ),
+              );
+            },
+            child: Text("Play Again"),
+          ),
+        ],
+      ),
     );
   }
 }
